b3b49fa0d233a798e98702469bc374498c8f44bf
Richard Challis <rjchallis@gmail.com>
2020-09-30 16:45:59 +0100

    add resultFields endpoint

---
+++ src/api/v0/routes/resultFields.js
@@ -0,0 +1,18 @@
+import { formatJson } from "../functions/formatJson";
+import { getRecordsById } from "../functions/getRecordsById";
+import { attrTypes } from "../functions/attrTypes";
+
+module.exports = {
+  getResultFields: async (req, res) => {
+    let fields = {};
+    let status = {};
+    try {
+      fields = await attrTypes();
+      status = { success: true };
+    } catch {
+      status = { success: false, error: "Unable to fetch fields" };
+    }
+    let response = { status, fields };
+    return res.status(200).send(formatJson(response, req.query.indent));
+  },
+};
