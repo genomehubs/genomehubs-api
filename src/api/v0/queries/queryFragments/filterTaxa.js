f85d158984b6eca4bfc4f9760bed6f5d4eec01fe
Richard Challis <richard.challis@sanger.ac.uk>
2021-08-24 09:10:56 +0100

    improve tax_name wildcard search

--- src/api/v0/queries/queryFragments/filterTaxa.js
+++ src/api/v0/queries/queryFragments/filterTaxa.js
@@ -19,19 +19,10 @@ export const filterTaxa = (depth, searchTerm, multiTerm, ancestral) => {
           should: multiTerm.map((term) => {
             let filter, should, must_not;
             let taxonId = [];
-            if (term.match(":")) {
-              let source;
-              [source, term] = term.split(":");
+            let source;
 
-              if (term && term > "*") {
-                filter = [
-                  {
-                    wildcard: {
-                      "taxon_names.name": term,
-                    },
-                  },
-                ];
-              }
+            if (term.match(":")) {
+              [source, term] = term.split(":");
               should = [
                 {
                   match: {
@@ -45,35 +36,66 @@ export const filterTaxa = (depth, searchTerm, multiTerm, ancestral) => {
                 },
               ];
             } else {
-              taxonId = [{ taxon_id: term }];
               must_not = {
                 exists: {
                   field: "taxon_names.source",
                 },
               };
             }
-            if (term.match(/\*/)) {
-              return {
-                bool: {
-                  should: [
-                    {
-                      nested: {
-                        path: "taxon_names",
-                        query: {
-                          bool: {
-                            filter,
-                            should,
-                            must_not,
-                          },
+            if (term && term > "*") {
+              if (term.match(/\*/)) {
+                filter = [
+                  {
+                    wildcard: {
+                      "taxon_names.name": term,
+                    },
+                  },
+                ];
+              } else {
+                filter = [
+                  {
+                    match: {
+                      "taxon_names.name": term,
+                    },
+                  },
+                ];
+                taxonId = [
+                  {
+                    match: {
+                      taxon_id: term,
+                    },
+                  },
+                ];
+              }
+            }
+            if (filter && should) {
+              filter.push({ bool: { should } });
+            } else if (should) {
+              filter = { bool: { should } };
+            }
+
+            // if (term.match(/\*/)) {
+            return {
+              bool: {
+                should: [
+                  {
+                    nested: {
+                      path: "taxon_names",
+                      query: {
+                        bool: {
+                          filter,
+                          // should,
+                          must_not,
                         },
                       },
                     },
-                  ]
-                    .concat(taxonId)
-                    .concat(lineage),
-                },
-              };
-            }
+                  },
+                ]
+                  .concat(taxonId)
+                  .concat(lineage),
+              },
+            };
+            // }
             return {
               bool: {
                 should: [
