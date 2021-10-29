50882eb0d5effedf697793e95654b0651f435623
Richard Challis <richard.challis@sanger.ac.uk>
2021-10-29 15:51:03 +0100

    timeout more quickly if memcached is not available

--- src/api/v2/functions/memcached.js
+++ src/api/v2/functions/memcached.js
@@ -3,9 +3,11 @@ const Memcached = require("memcached-promise");
 import { config } from "./config";
 
 export const mc = config.memcached
-  ? new Memcached("localhost:12345", {
+  ? new Memcached(config.memcached, {
       maxExpiration: 2592000,
       namespace: `${config.hub}${config.separator}${config.release}`,
       debug: false,
+      timeout: 1000,
+      retries: 2,
     })
   : undefined;
