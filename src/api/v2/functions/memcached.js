9142e81ef017f480d5f7435f9aa2d2a34672de25
Richard Challis <richard.challis@sanger.ac.uk>
2021-11-26 16:22:05 +0000

    bug fix

--- src/api/v2/functions/memcached.js
+++ src/api/v2/functions/memcached.js
@@ -9,6 +9,6 @@ export const mc = config.memcached
       debug: false,
       timeout: 1000,
       retries: 2,
-      maxValue=1024*1024*32,
+      maxValue: 1024 * 1024 * 32,
     })
   : undefined;
